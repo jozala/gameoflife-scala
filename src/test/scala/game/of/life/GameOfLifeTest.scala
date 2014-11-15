802c05bb7b8b442b5b449da49669c83d89d3c567
mariusz <mariusz@aetas.pl>
2014-11-15 14:42:23 +0000

    hor

--- src/test/scala/game/of/life/GameOfLifeTest.scala
+++ src/test/scala/game/of/life/GameOfLifeTest.scala
@@ -21,11 +21,23 @@ class GameOfLifeTest extends FlatSpec with Matchers with MockitoSugar {
     val board=new Board(Cell(1,1), Cell(1,2))
     board.countNeighbours(1,1) should be (1)
   }
+
+  "Board" should "calculate live neighbours horizontally" in {
+    val board=new Board(Cell(1,1), Cell(2,1),Cell(0,1))
+    board.countNeighbours(1,1) should be (2)
+  }
+
 }
 
 class Board(cells: Cell*) {
   def countNeighbours(x: Int, y: Int): Int = {
-    return 1
+    var sum=0
+    if (isAlive(x-1,y)) sum += 1
+    if (isAlive(x+1,y)) sum += 1
+    if (isAlive(x,y+1)) sum += 1
+    if (isAlive(x,y-1)) sum += 1
+
+    return sum
   }
 
   def isAlive(x: Int, y: Int)= { if (cells.contains(Cell(x,y))) true else false}
