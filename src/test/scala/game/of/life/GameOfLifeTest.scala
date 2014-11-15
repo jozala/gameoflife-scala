4a0777f8d8f16aa132148202efd83be94e51749c
mariusz <mariusz@aetas.pl>
2014-11-15 14:27:18 +0000

    cnot spec

--- src/test/scala/game/of/life/GameOfLifeTest.scala
+++ src/test/scala/game/of/life/GameOfLifeTest.scala
@@ -12,14 +12,23 @@ class GameOfLifeTest extends FlatSpec with Matchers with MockitoSugar {
     val board = new Board(Cell(1,1))
     board.isAlive(1,1) shouldBe true
   }
-"Cell not specified on the board" should "be dead" in {
-  val board=new Board(Cell(0,1))
-  board.isAlive(1,1) shouldBe false
-}
+  "Cell not specified on the board" should "be dead" in {
+    val board=new Board(Cell(0,1))
+    board.isAlive(1,1) shouldBe false
+  }
+
+  "Board" should "calculate live neighbours vertically" in {
+    val board=new Board(Cell(1,1), Cell(1,2))
+    board.countNeighbours(1,1) should be (1)
+  }
 }
 
 class Board(cells: Cell*) {
-  def isAlive(x: Int, y: Int)= { if (cells.contains(Cell(x,y))) true else false} 
+  def countNeighbours(x: Int, y: Int): Int = {
+    return 1
+  }
+
+  def isAlive(x: Int, y: Int)= { if (cells.contains(Cell(x,y))) true else false}
 
 
 }
