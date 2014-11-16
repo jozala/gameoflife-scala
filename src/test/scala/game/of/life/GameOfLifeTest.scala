58ee4a23573dee990af1a614a529517ac96052ca
mariusz <mariusz@aetas.pl>
2014-11-16 13:32:50 +0000

    GameOfLife is ready

--- src/test/scala/game/of/life/GameOfLifeTest.scala
+++ src/test/scala/game/of/life/GameOfLifeTest.scala
@@ -6,54 +6,99 @@ import org.scalatest.{Matchers, FlatSpec}
 
 import scala.collection.mutable
 
-class GameOfLifeTest extends FlatSpec with Matchers with MockitoSugar {
+class GameOfLifeTest extends FlatSpec with Matchers {
 
-  "Cell specified on the board" should "be alive" in {
-    val board = new Board(Cell(1,1))
-    board.isAlive(1,1) shouldBe true
+  "Cell specified on the board" should "be seen alive" in {
+    val board = new Board(Cell(1, 1))
+    board.isAlive(1, 1) shouldBe true
   }
-  "Cell not specified on the board" should "be dead" in {
-    val board=new Board(Cell(0,1))
-    board.isAlive(1,1) shouldBe false
-  }
-
-  "Board" should "calculate live neighbours vertically" in {
-    val board=new Board(Cell(1,1), Cell(1,2))
-    board.countNeighbours(1,1) should be (1)
+  "Cell not specified on the board" should "be seen dead" in {
+    val board = new Board(Cell(0, 1))
+    board.isAlive(1, 1) shouldBe false
   }
 
-  "Board" should "calculate live neighbours horizontally" in {
-    val board=new Board(Cell(1,1), Cell(2,1),Cell(0,1))
-    board.countNeighbours(1,1) should be (2)
+  it should "count live neighbours vertically" in {
+    val board = new Board(Cell(3, 5), Cell(3, 7))
+    board.countLiveNeighbours(Cell(3, 6)) should be(2)
+  }
+
+  it should "count live neighbours horizontally" in {
+    val board = new Board(Cell(3, 3), Cell(4, 3), Cell(2, 3))
+    board.countLiveNeighbours(Cell(3, 3)) should be(2)
   }
 
   "Board" should "calculate live neighbours diagonally" in {
-    val board=new Board(Cell(0,0),Cell(2,2), Cell(0,2), Cell(2,0))
-    board.countNeighbours(1,1) should be (4)
+    val board = new Board(Cell(5, 5), Cell(7, 7), Cell(5, 7), Cell(7, 5))
+    board.countLiveNeighbours(Cell(6, 6)) should be(4)
   }
 
+  "Live cell" should "stay alive when it has 2 neighbours" in {
+    val board = new Board(Cell(3, 3), Cell(4, 4), Cell(4, 3))
+    val newBoard = board.next()
+    newBoard.isAlive(4, 4) shouldBe true
+  }
+
+  it should "stay alive when it has 3 neighbours" in {
+    val board = new Board(Cell(2, 4), Cell(3, 5), Cell(3, 4), Cell(2, 5))
+    val newBoard = board.next()
+    newBoard.isAlive(3, 5) shouldBe true
+  }
+
+  it should "die when it has more than 3 neighbours" in {
+    val board = new Board(Cell(3, 3), Cell(4, 4), Cell(4, 3), Cell(3, 4), Cell(5, 4))
+    val newBoard = board.next()
+    newBoard.isAlive(4, 4) shouldBe false
+  }
+
+  it should "die when it has less than 2 neighbours" in {
+    val board = new Board(Cell(2, 8), Cell(2, 7))
+    val newBoard = board.next()
+    newBoard.isAlive(2, 8) shouldBe false
+  }
+
+  "Dead cell" should "become alive when it has exactly 3 neighbours" in {
+    val board = new Board(Cell(2, 3), Cell(1, 3), Cell(3, 5 ))
+    val newBoard = board.next()
+    newBoard.isAlive(2, 4) shouldBe true
+  }
+
+  "Cell" should "give all neighbours" in {
+    Cell(5, 5).getNeighbours should contain allOf(Cell(4, 4), Cell(5, 4), Cell(6, 4),
+      Cell(4, 5), Cell(6, 5),
+      Cell(4, 6), Cell(5, 6), Cell(6, 6))
+    Cell(5, 5).getNeighbours shouldNot contain(Cell(5, 5))
+  }
 }
+
 
 class Board(cells: Cell*) {
 
-  def countNeighbours(x: Int, y: Int): Int = {
-    var sum=0
-    if (isAlive(x-1,y)) sum += 1
-    if (isAlive(x+1,y)) sum += 1
-    if (isAlive(x,y+1)) sum += 1
-    if (isAlive(x,y-1)) sum += 1
+  def countLiveNeighbours(cell: Cell): Int = cells.count(_.isNeighbour(cell))
+  def isAlive(x: Int, y: Int) = cells.contains(Cell(x,y))
+  def next(): Board = {
 
-    if (isAlive(x-1,y-1)) sum += 1
-    if (isAlive(x+1,y+1)) sum += 1
-    if (isAlive(x+1,y-1)) sum += 1
-    if (isAlive(x-1,y+1)) sum += 1
+    val cellsToCheck: Seq[Cell] = cells.flatMap(cell => {
+        cell.getNeighbours :+ cell
+    })
 
-    return sum
+    new Board(cellsToCheck.filter(cell => {
+        val neighbourNumber: Int = countLiveNeighbours(cell)
+        neighbourNumber >= 2 && neighbourNumber <= 3
+      }):_*
+    )
   }
-
-  def isAlive(x: Int, y: Int)= cells.contains(Cell(x,y))
-
-
 }
 
-case class Cell(x: Int, y: Int)
+case class Cell(x: Int, y: Int) {
+  def isNeighbour(cell: Cell): Boolean = getNeighbours.contains(cell)
+  
+  def getNeighbours: Seq[Cell] = {
+    for {
+      x <- -1 to 1
+      y <- -1 to 1
+      if x != 0 || y != 0
+    } yield {
+      new Cell(this.x + x, this.y + y)
+    }
+  }
+}
